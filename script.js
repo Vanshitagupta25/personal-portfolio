74d2fb9155a711b71e551fcf5eb21e68e471bb14
vanshitagupta25 <vanshitagupta906@gmail.com>
2026-01-26 23:06:45 +0530

    Initial protfolio commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+  strings: ["Frontend Developer", "MERN Stack Developer", "Full Stack Developer"],
+  typeSpeed: 100,
+  backSpeed: 80,
+  backDelay: 1000,
+  loop: true
+});
